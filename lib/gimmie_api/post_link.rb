01f7dfe87ecea8a31ba6267756944030410c7cca
John Wilkinson <jcwilk@gmail.com>
2016-10-09 20:25:04 +0800

    Add to_h/to_s to make link more transparent

--- lib/gimmie_api/post_link.rb
+++ lib/gimmie_api/post_link.rb
@@ -8,5 +8,14 @@ module GimmieAPI
     def post
       Hashie::Mash.new(@client.post(@link)._attributes.to_h)
     end
+
+    def to_h
+      {'url' => @link._url}
+    end
+    alias_method :to_hash, :to_h
+
+    def to_s
+      to_h.to_s
+    end
   end
 end
